66c9f09fb15f2196c13f85ab98c95435d9a5e544
Kerrie Yee <kerrie@thredup.com>
2019-10-23 14:02:10 -0700

    adding binary tree and tree implementations

--- trees/binary_tree.rb
+++ trees/binary_tree.rb
@@ -1,8 +1,80 @@
 class BinaryTree
+  attr_reader :root
 
+  def initialize
+    @root = nil
+  end
+
+  def insert(key)
+    #inserting at top
+    return if key.nil?
+    new_node = BinaryTreeNode.new(key)
+    return @root = new_node if root.nil?
+
+    parent_node = find_parent(root, new_node)
+
+    if new_node.key < parent_node.key
+      parent_node.left = new_node
+    else
+      parent_node.right = new_node
+    end
+  end
+
+  def in_order_traversal(start = root.dup)
+    return if start.nil?
+    in_order_traversal(start.left) if start.left
+    p start.key
+    in_order_traversal(start.right) if start.right
+  end
+
+  def pre_order_traversal(start = root.dup)
+    return if start.nil?
+    p start.key
+    pre_order_traversal(start.left) if start.left
+    pre_order_traversal(start.right) if start.right
+  end
+
+  def post_order_traversal(start = root.dup)
+    return if start.nil?
+    post_order_traversal(start.left) if start.left
+    post_order_traversal(start.right) if start.right
+    p start.key
+  end
+
+  def breadth_first_level_traversal
+    return if root.nil?
+    queue = [root]
+    while !queue.empty? do
+      n = queue.shift
+      p n.key
+      queue << n.left if n.left
+      queue << n.right if n.right
+    end
+  end
+
+  private
+
+  def find_parent(node, new_node, direction = 'center')
+    return node if node.nil?
+
+    if new_node.key < node.key
+      return node unless node.left
+      find_parent(node.left, new_node, 'left')
+    else
+      return node unless node.right
+      find_parent(node.right, new_node, 'right')
+    end
+  end
 end
 
 class BinaryTreeNode
+  attr_reader :key
+  attr_accessor :left, :right
 
+  def initialize(key)
+    @key = key
+    @right = nil
+    @left = nil
+  end
 end
 
--- trees/tree.rb
+++ trees/tree.rb
@@ -1,8 +1,57 @@
 class Tree
+  attr_reader :root
 
+  def initialize
+    @root = nil
+  end
+
+  def add_node(parent_node, new_node)
+    #inserting at top
+    if parent_node.nil?
+      return @root = new_node if root.nil?
+      new_node.children << @root
+      @root = new_node
+    else
+      parent_node.children << new_node
+    end
+  end
+
+  def pre_order_traversal(start = root.dup)
+    return if start.nil?
+    p start.key
+    start.children.each do | child |
+      pre_order_traversal(child)
+    end
+  end
+
+  def post_order_traversal(start = root.dup)
+    return if start.nil?
+    start.children.each do | child |
+      post_order_traversal(child)
+    end
+    p start.key
+  end
+
+  def breadth_first_level_traversal
+    return if root.nil?
+    queue = [root]
+    while !queue.empty? do
+      n = queue.shift
+      p n.key
+      n.children.each do |c|
+        queue << c
+      end
+    end
+  end
 end
 
 class TreeNode
+  attr_reader :key
+  attr_accessor :children
 
+  def initialize(key)
+    @key = key
+    @children = []
+  end
 end
 
